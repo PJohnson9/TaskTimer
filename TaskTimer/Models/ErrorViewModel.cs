89be1e6e4d80c72e59e34c6ca7f4e880a67be7cd
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2021-10-23 19:04:40 -0400

    Add project files.

---
+++ TaskTimer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskTimer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
