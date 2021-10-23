f64aacbf1137302fb5261cecdaa503079fd03785
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2021-10-23 19:45:24 -0400

    Model Classes

---
+++ TaskTimer/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTimer.Models
+{
+    public class Task
+    {
+        //[Key]
+        //public Int32 ContactID { get; set; }
+
+
+        [Required]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+
+        [Required]
+        [Display(Name = "E-Mail Address")]
+        [EmailAddress(ErrorMessage = "Invalid E-Mail Address")]
+        public String EMail { get; set; }
+    }
+}
